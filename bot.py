036e2bddc44cc52b5ee68ab387836062a9899a7c
dolphinxyz <robimalco@gmail.com>
2021-12-29 15:20:47 +0100

    improve

--- bot.py
+++ bot.py
@@ -1,33 +1,64 @@
 import os
+import sys
 import time
+import json
 import discord
 import requests
 import threading
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
-
-
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN_TEST = os.getenv('DISCORD_TOKEN_TEST')
+COINMARKETCAP_TOKEN = os.getenv('COINMARKETCAP_TOKEN')
+MM_CMC_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+MM_CMC_PARAMS = {'slug':'million'}
+
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': COINMARKETCAP_TOKEN,
+}
 
 client = discord.Client()
 
 price = 0
-mm_url = "https://coinmarketcap.com/currencies/million/"
-def ExtractMmPrice():
-    global price
+volume = 0
+rank = 0
+def ExtractCoinMarketCap():
+    global price, volume, rank
     while True:
-        page = requests.get(mm_url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        price = soup.find_all("div", {"class": "priceValue"})[0].getText()
-        print(price)
-        time.sleep(60)
+        try:
+            response = requests.get(
+                MM_CMC_URL,
+                params=MM_CMC_PARAMS,
+                headers=headers
+            )
+            data = json.loads(response.text)
+            price = data['data']['10866']['quote']['USD']['price']
+            volume = data['data']['10866']['quote']['USD']['volume_24h']
+            rank = data['data']['10866']['cmc_rank']
+        except (ConnectionError, Timeout, TooManyRedirects) as e:
+            print("ERROR:\n\t", e)
+        time.sleep(120)
 
 @client.event
 async def on_message(message):
     if message.content.startswith('!price'):
-        await message.reply(price, mention_author=True)
+        r_price = round(price, 2)
+        s_price = str(r_price) + '$'
+        await message.reply(s_price, mention_author=True)
+    if message.content.startswith('!volume'):
+        r_volume = round(volume)
+        s_volume = f"${r_volume:,}"
+        await message.reply(s_volume, mention_author=True)
+    if message.content.startswith('!rank'):
+        await message.reply(rank, mention_author=True)
 
-threading.Thread(target=ExtractMmPrice).start()
-client.run(TOKEN)
+if __name__ == "__main__":
+    mode = sys.argv[1]
+    threading.Thread(target=ExtractCoinMarketCap).start()
+    if mode == "prod":
+        client.run(TOKEN)
+    if mode == "test":
+        client.run(TOKEN_TEST)
+
